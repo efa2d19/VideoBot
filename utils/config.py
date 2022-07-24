69724d7528cf0091d68124dde7da218b607d48a7
Drugsosos <44712637+Drugsosos@users.noreply.github.com>
2022-07-24 21:35:52 +0300

    rewrote config in toml

---
+++ utils/config.py
@@ -0,0 +1,216 @@
+import toml
+from os.path import exists
+from rich.console import Console
+from typing import Optional, Union, TypeVar, Callable
+
+from utils.console import handle_input
+
+function = TypeVar("function", bound=Callable[..., object])
+
+console = Console()
+config: Optional[dict] = None
+config_name: str = "config.toml"
+config_template_name: str = ".config.template.toml"
+
+
+def crawl(
+        obj: dict,
+        func: function = lambda x, y: print(x, y),
+        path: Optional[list] = None,
+) -> None:
+    """
+    Crawls on values of the dict and executes func if found dict w/ settings
+
+    Args:
+        obj: Dict to be crawled on
+        func: Function to be executed with settings
+        path: List with keys of nested dict
+    """
+    if not path:
+        path = []
+
+    for key, value in obj.items():
+        if type(value) is dict and any([type(v) is dict for v in value.values()]):
+            crawl(value, func, path + [key])
+            continue
+        func(path + [key], value)
+
+
+def check(
+        value: any,
+        checks: dict,
+        name: str,
+):
+    correct = True if value else False
+
+    if correct and "type" in checks:
+        try:
+            value = eval(checks["type"])(value)
+        except Exception:  # noqa (Exception is fine, it's not too broad)
+            correct = False
+
+    if not correct:
+        default_values = {
+            "explanation": "",
+            "var_type": "False",
+            "default": NotImplemented,
+            "regex": "",
+            "input_error": "Incorrect input",
+            "nmin": None,  # noqa
+            "nmax": None,  # noqa
+            "oob_error": "Input out of bounds(Value too high/low/long/short)",
+            "options": None,
+            "optional": False,
+        }
+
+        [checks.update({key: value}) for key, value in default_values.values() if not checks.get(key)]
+
+        value = handle_input(name=name, **checks)
+    return value
+
+
+def nested_get(
+        obj: dict,
+        keys: list,
+) -> any:
+    """
+    Gets value from nested dict by list with path
+
+    Args:
+        obj: Nested dict
+        keys: List with path
+    Return:
+        Value of last key
+    """
+    for key in keys:
+        obj = obj.get(key, {})
+    return obj
+
+
+def nested_set(
+        obj: dict,
+        keys: list,
+        value: any,
+) -> None:
+    """
+    Sets last key in the nested dict by the path
+
+    Args:
+        obj: Nested dict
+        keys: List with path
+        value: Value to set
+    """
+    for key in keys[:-1]:
+        obj = obj.setdefault(key, {})
+    obj[keys[-1]] = value
+
+
+def check_vars(
+        path: list,
+        checks: dict,
+) -> None:
+    """
+    Checks if value is in nested dict and correct by path of keys
+
+    Args:
+        path: List with path
+        checks: Dict with all checks
+    """
+    global config
+    if checks is None:
+        checks = dict()
+
+    value = check(
+        nested_get(config, path),
+        checks,
+        name=path[-1],
+    )
+    nested_set(config, path, value)
+
+
+def check_config_wrapper(
+        func: function,
+) -> function:
+    """
+    Exception wrapper for check_config function
+    """
+
+    def wrapper(*args, **kwargs):
+        if args:
+            kwargs["name"] = args[0]
+            if args.__len__() > 1:
+                kwargs["template_name"] = args[-1]
+        if not kwargs or not all(arg is not None for arg in kwargs.values()):
+            kwargs["name"] = config_name
+            kwargs["template_name"] = config_template_name
+
+        try:
+            return func(*args, **kwargs)
+        except toml.TomlDecodeError:
+            if console.input(f"[blue]Couldn't read {kwargs['name']}.\nOverwrite it?(y/n)").startswith("y"):
+                try:
+                    with open(kwargs["name"], "w") as f:
+                        f.write("")
+                    return func(*args, **kwargs)
+                except Exception:  # noqa (Exception is fine, it's not too broad)
+                    console.print(
+                        f"[red bold]Failed to overwrite {kwargs['name']}. Giving up.\n"
+                        f"Suggestion: check {kwargs['name']} permissions for the user."
+                    )
+                    return False
+            console.print("Unable to read config, and not allowed to overwrite it. Giving up.")
+            return False
+        except Exception as error:
+            console.print(f"[red bold]Encountered error when trying to to load {kwargs['template_name']}: {error}")
+            return False
+
+    return wrapper
+
+
+@check_config_wrapper
+def check_config(
+        name: Optional[str] = None,
+        template_name: Optional[str] = None,
+) -> Union[dict, bool]:
+    """
+    Checks config and returns corrected version
+
+    Args:
+        name: Config name
+        template_name: Template name
+    Return:
+        Corrected config file as a dict
+    """
+    if not name:
+        name = config_name
+    if not template_name:
+        template_name = config_template_name
+
+    global config
+
+    if not exists(name):
+        console.print(f"[blue]Couldn't find {name}\nCreating it now.")
+        try:
+            with open(name, "w+") as f:
+                f.write("")
+            config = dict()
+        except Exception:  # noqa (Exception is fine, it's not too broad)
+            console.print(
+                f"[red bold]Failed to write to {name}.Giving up.\n"
+                f"Suggestion: check the folder's permissions for the user."
+            )
+            return False
+    else:
+        config = toml.load(config_name)
+
+    template = toml.load(template_name)
+    crawl(template, check_vars)
+    with open(config_name, "w") as f:
+        toml.dump(config, f)
+    return config
+
+
+if __name__ == "__main__":
+    print(check_config())
+    # template = toml.load(config_template_name)
+    # crawl(template)
---
+++ utils/console.py
@@ -0,0 +1,104 @@
+from rich.console import Console
+import re
+from typing import Optional, Union
+
+console = Console()
+
+
+def handle_input(
+        *,
+        var_type: Optional[str] = None,
+        regex: str = "",
+        input_error: str = "",
+        nmin: Optional[int] = None,  # noqa
+        nmax: Optional[int] = None,  # noqa
+        oob_error: str = "",
+        explanation: str = "",
+        options: list = None,
+        default: Union[str, NotImplemented] = NotImplemented,
+        optional: bool = False,
+        example: Optional[str] = None,
+        name: str = "",
+        message: Optional[str, float] = None,
+):
+    if not message:
+        message = (
+                (("[blue]Example: " + str(example) + "\n") if example else "")
+                + "[red]"
+                + ("Non-optional ", "Optional ")[optional]
+                + "[#C0CAF5 bold]"
+                + str(name)
+                + "[#F7768E bold]="
+        )
+    var_type = eval(var_type)
+
+    if optional:
+        console.print(message + "\n[green]This is an optional value. Do you want to skip it? (y/n)")
+        if input().casefold().startswith("y"):
+            return default if default is not NotImplemented else ""
+    if default is not NotImplemented:
+        console.print(
+            "[green]"
+            + message
+            + '\n[blue bold]The default value is "'
+            + str(default)
+            + '"\nDo you want to use it?(y/n)'
+        )
+        if input().casefold().startswith("y"):
+            return default
+    if options is None:
+        regex = re.compile(regex)
+        console.print("[green bold]" + explanation, no_wrap=True)
+        while True:
+            console.print(message, end="")
+            user_input = input("").strip()
+            if var_type:
+                try:
+                    user_input = var_type(user_input)
+                    if (nmin is not None and user_input < nmin) or (
+                            nmax is not None and user_input > nmax
+                    ):
+                        # FAILSTATE Input out of bounds
+                        console.print("[red]" + oob_error)
+                        continue
+                    break  # Successful type conversion and number in bounds
+                except ValueError:
+                    # Type conversion failed
+                    console.print("[red]" + input_error)
+                    continue
+            elif regex != "" and re.match(regex, user_input) is None:
+                console.print("[red]" + input_error + "\nAre you absolutely sure it's correct?(y/n)")
+                if input().casefold().startswith("y"):
+                    break
+                continue
+            else:
+                # FAILSTATE Input STRING out of bounds
+                if (nmin is not None and len(user_input) < nmin) or (
+                        nmax is not None and len(user_input) > nmax
+                ):
+                    console.print("[red bold]" + oob_error)
+                    continue
+                break  # SUCCESS Input STRING in bounds
+        return user_input
+    console.print(explanation, no_wrap=True)
+    while True:
+        console.print(message, end="")
+        user_input = input("").strip()
+        if var_type is not False:
+            try:
+                isinstance(eval(user_input), var_type)
+                return var_type(user_input)
+            except Exception:  # noqa (Exception is fine, it's not too broad)
+                console.print(
+                    "[red bold]"
+                    + input_error
+                    + "\nValid options are: "
+                    + ", ".join(map(str, options))
+                    + "."
+                )
+                continue
+        if user_input in options:
+            return user_input
+        console.print(
+            "[red bold]" + input_error + "\nValid options are: " + ", ".join(map(str, options)) + "."
+        )
